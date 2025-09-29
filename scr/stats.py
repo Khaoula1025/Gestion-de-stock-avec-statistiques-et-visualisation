98a42018b86ed1cf88fa48d5e0f3b9da65dc61b6
SaidaAourras <sa123aouras@gmail.com>
2025-09-29 22:59:49 +0100

    add val_total_stock function

---
+++ scr/stats.py
@@ -0,0 +1,13 @@
+import numpy as np
+import data
+
+products = data.products
+
+def val_total_stock(products):
+    quantite = products[:,1]
+    prix = products[:,2]
+    valeur_total = np.add.reduce(quantite * prix)
+    print(quantite * prix)
+    return valeur_total
+    
+print(val_total_stock(products))
