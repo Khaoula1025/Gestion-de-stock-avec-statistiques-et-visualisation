3f4fe5022a14f1c70860c272ac7d3de73c59105c
SaidaAourras <sa123aouras@gmail.com>
2025-09-29 23:01:49 +0100

    commenter une ligne

--- scr/stats.py
+++ scr/stats.py
@@ -7,7 +7,7 @@ def val_total_stock(products):
     quantite = products[:,1]
     prix = products[:,2]
     valeur_total = np.add.reduce(quantite * prix)
-    print(quantite * prix)
+    # print(quantite * prix)
     return valeur_total
     
 print(val_total_stock(products))
