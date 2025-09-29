788fab130b5d24b381514028e386c380d2c8b881
Khaoula Esioudi <khaoula.esioudi@gmail.com>
2025-09-29 15:33:45 +0100

    refactore mettre a jour function

--- scr/stock.py
+++ scr/stock.py
@@ -1,10 +1,13 @@
 def mettreAjourQ(n):
-    products=n
     choix=input('enter product name : ')
+    found=False
     for i in n:
-        if choix not in products[i[0]] :
+        if i[0].lower()==choix.lower() :
+             q=int(input('enter the new quantity : '))
+             i[1]=q
+             found=True
+             break
+    if not found :
             print('product not found ')
-        else:
-            q=int(print('enter the new quantity : '))
-            products[i[1]]=q
-    return products
+           
+    return n
