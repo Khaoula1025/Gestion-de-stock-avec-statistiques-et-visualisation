fc2998778d2d405749065d7fe85e279abe9400af
Khaoula Esioudi <khaoula.esioudi@gmail.com>
2025-09-29 14:07:20 +0100

    added requirement.txt fileand data.py

---
+++ scr/data.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+stock = np.array([
+    ["Stylo", 50, 2.5],
+    ["Cahier", 30, 12.0],
+    ["Sac à dos", 15, 250.0],
+    ["Bouteille d'eau", 100, 5.0],
+    ["Clé USB", 40, 75.0],
+    ["Souris", 20, 120.0],
+    ["Clavier", 10, 300.0],
+    ["Lampe de bureau", 12, 150.0],
+    ["Agenda", 25, 35.0],
+    ["Calculatrice", 18, 200.0],
+], dtype=object)
