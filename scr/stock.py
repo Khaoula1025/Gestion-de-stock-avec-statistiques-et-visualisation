6ea86bc575cf75c89a6cd4b6662006df79869366
maryem <maryemelbergui@gmail.com>
2025-09-29 15:45:37 +0100

    afficher produits

--- scr/stock.py
+++ scr/stock.py
@@ -3,10 +3,14 @@ import data as produit
 
 products =produit.products
 def ajouter_produit():
+    global products
     nom_produit= input("inserer nom produit: ")
     prix= input("inserer le prix du produit: ")
     quantite= int(input("inserer la quantite du produit"))
     new_produit= [nom_produit, prix, quantite]
     products = np.vstack([produit.products , new_produit])
     print(products)
-ajouter_produit()
+
+print('product added')
+def produit_disponible():
+    return products
