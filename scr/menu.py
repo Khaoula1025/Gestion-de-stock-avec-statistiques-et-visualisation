a981ca3a6977cf7e8c01c2ab9fa270acbac3c421
SaidaAourras <sa123aouras@gmail.com>
2025-09-30 10:35:39 +0100

    manage conflict

---
+++ scr/menu.py
@@ -0,0 +1,10 @@
+def menu():
+    print('1. Ajouter un produit. ')
+    print('2. Supprimer un produit. ')
+    print('3. Modifier la quantité d’un produit. ')
+    print('4. Afficher le stock. ')
+    print('5. Afficher les statistiques. ')
+    print('6. Afficher un graphique (choix : bar chart ou pie chart). ')
+    print('7. Quitter.')
+    
+    
