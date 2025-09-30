723c8066638f2f904df544149528d6a81c85cafb
maryem <maryemelbergui@gmail.com>
2025-09-30 10:26:58 +0100

    create pie chart

---
+++ scr/stats.py
@@ -0,0 +1,32 @@
+import numpy as np
+import data
+import stock as stock
+products = stock.products
+
+def val_total_stock(products):
+    quantite = products[:,1]
+    prix = products[:,2]
+    valeur_total = np.add.reduce(quantite * prix)
+    # print(quantite * prix)
+    return valeur_total
+
+
+def prix_min_max():
+     global products
+     count=0   
+     for p in products:
+          if count == 0:
+             prix_min= float(p[2])
+             prix_max= float(p[2])
+             count= count+1
+          elif float(p[2])<prix_min:
+               prix_min = float(p[2])
+               count= count+1
+          elif float(p[2])>= prix_max:
+               prix_max=float(p[2])
+               count= count+1
+     return f"Prix maximum {prix_max} , Prix minimum {prix_min}"
+
+ 
+print(val_total_stock(products))
+print(prix_min_max())
--- scr/stock.py
+++ scr/stock.py
@@ -1,6 +1,5 @@
 import numpy as np
 import data as produit
-import matplotlib.pyplot as plt
 def mettreAjourQ(n):
     choix=input('enter product name : ')
     found=False
@@ -25,31 +24,23 @@ def ajouter_produit():
     products = np.vstack([produit.products , new_produit])
     print(products)
 
-def produit_disponible():
-    return products
-def prix_min_max():
-     global products
-     count=0   
-     for p in products:
-          if count == 0:
-             prix_min= float(p[2])
-             prix_max= float(p[2])
-             count= count+1
-          elif float(p[2])<prix_min:
-               prix_min = float(p[2])
-               count= count+1
-          elif float(p[2])>= prix_max:
-               prix_max=float(p[2])
-               count= count+1
-     return f"Prix maximum {prix_max} , Prix minimum {prix_min}"
 
-def pie_chart():
-     pr_name= []
-     val_pr= []
-     for p in products:
-        pr_name.append(p[0])
-        val_pr.append(p[1]*p[2])
+def delete_product():
     
-     plt.pie(val_pr, labels= pr_name)
-     plt.show()
+    product_name = input("donner le nom du produit : ")
+    product_index = None
+    for i,product in enumerate(products):
+        if product[0].lower() == product_name.lower():
+            product_index =i
+            break 
+        
+    if product_index is not None:
+        new_list = np.delete(products, product_index,axis=0)
+        return new_list
+    else:
+        return 'produit introuvable'
+# print(delete_product(products))
+
+
+
 
